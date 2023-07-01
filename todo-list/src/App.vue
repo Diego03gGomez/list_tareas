e27984d7fefc0b30427434f3fbd7c445b75e9ce6
Diego <ezequielgomezdie@gmail.com>
2023-06-30 21:26:59 -0300

    primer commit

---
+++ todo-list/src/App.vue
@@ -0,0 +1,72 @@
+<script setup>
+
+import lista from  './components/lista.vue';
+</script>
+
+
+
+
+
+
+
+<template>
+<div class="contenido">
+  <div class="explicacion">
+    <h1>Lista de tareas</h1>
+<h4>Ingrese sus tareas en el input y agreguelas</h4>
+  </div>
+
+
+<lista></lista>
+
+
+  
+  
+
+</div>
+</template>
+
+<style scoped>
+
+.contenido{
+  background-color: #080f28;
+  margin: auto;
+  width: 45%;
+  text-align: center;
+}
+.explicacion{
+  background-color: #080f28;
+ color: white;
+ font-size: 30px;
+}
+
+@media (max-width:1024px){
+  .contenido{
+    width: 90%;
+   
+  
+  }
+}
+
+@media(max-width:589px){
+  .contenido{
+    text-align: center;
+  }
+}
+
+
+
+
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ todo-list/src/components/lista.vue
@@ -0,0 +1,126 @@
+<template lang="">
+    <div>
+    <div class="div_input">
+        <input  v-model="valor_input" type="text" placeholder= "Ingrese su tarea"> <label for=""><button ref="boton"  v-on:click="agregar()" class="agregar"><i class="fa-solid fa-plus"></i></button></label>
+    </div>   
+
+
+
+<div class="tareas"  >
+<div  v-for="(e, index) in tareas" :key="index" class="tarea">
+    <h1> {{e.nombre}}  </h1>
+<eliminar></eliminar>
+</div>
+</div>
+
+
+
+
+    </div>
+</template>
+
+
+<script>
+import eliminar from './eliminar.vue';
+export default {
+    components: {
+    eliminar,
+},
+data() {
+    return {
+
+        tareas: [/*
+{nombre:"pepe", edad: 36},
+{nombre:"caca", edad: 20},
+{nombre:"jaja", edad: 12}*/
+],
+
+
+
+valor_input :"",
+
+
+
+    }
+},
+
+methods: {
+    agregar() {
+if(this.valor_input == null){
+
+this.$refs.boton.disabled = false;
+
+}else{
+    console.log(this.valor_input);
+this.tareas.push({nombre: this.valor_input, edad: 0});
+this.valor_input = "";
+}
+
+
+
+    }
+},
+created () {
+
+},
+
+
+    
+}
+</script>
+
+
+
+<style scoped>
+
+.div_input{
+    background-color: #080f28;
+    margin: auto;
+    width: 70%;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    padding: 10px 0;
+}
+
+.agregar{
+    background-color: blueviolet;
+    border-color: blueviolet;
+    color: white;
+    font-weight: bold;
+    border-style: solid;
+    cursor: pointer;
+    font-size: 30px;
+padding: 0px 20px;
+}
+
+.div_input input{
+    width: 80%;
+    font-size: 20px;
+    padding: 10px 0px;
+}
+
+.tarea{
+    background-color: blueviolet;
+    margin: 10px 0;
+    padding: 10px 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: white;
+}
+
+
+@media (max-width:1024px){
+.div_input{
+    width: 100%;
+}
+
+.div_input input{
+    width: 60%;
+}
+
+}
+
+    
+</style>
